11292de414ea21a92cdfba110a6d7cdd52ba89b9
Ismail Dicky S <ismaildicky984@gmail.com>
2024-11-15 23:36:19 +0700

    feat(function): Created a function isEven

---
+++ exercises/checking-if-a-number-is-even/index.js
@@ -0,0 +1,12 @@
+function isEven (Number) {
+            if (Number % 2 === 0) {
+                console.log("True");
+            } else {
+                console.log("False");
+            }
+        }
+
+        isEven(45);
+        isEven(30);
+        isEven(19);
+        isEven(10);
