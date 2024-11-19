0c846e6f9cdf1ade552cfed0cabeb2689427c392
Ismail Dicky S <ismaildicky984@gmail.com>
2024-11-19 17:57:03 +0700

    feat(arrayMethod): Use the .includes method to check whether the animal cat is in the array

---
+++ exercises/checking-elements-in-an-array-using-includes/README.md
@@ -0,0 +1,6 @@
+# Description:
+Create an array animals that contains several animal names.
+Use the .includes() method to check whether the animal "cat" is in the array, and display the result in the console.
+
+# Concept:
+Array, .includes() Method.
---
+++ exercises/checking-elements-in-an-array-using-includes/index.js
@@ -0,0 +1,10 @@
+let animals = ["Cat", "Dog", "Horse","Elephant", "Penguin", "Shark", "Monkey"];
+
+let check = animals.includes("Cat");
+console.log(check);
+
+let check2 = animals.includes("Dog");
+console.log(check2)
+
+let check3 = animals.includes("Zebra");
+console.log(check3);
