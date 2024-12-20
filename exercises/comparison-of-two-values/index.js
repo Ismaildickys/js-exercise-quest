ddd8368be5543a6a0456649b463b73e936693772
Ismail Dicky S <ismaildicky984@gmail.com>
2024-12-20 10:13:20 +0700

    feat(dataTypeBasics): Create a compareNumber function that accepts two parameters that are numbers

---
+++ exercises/comparison-of-two-values/index.js
@@ -0,0 +1,11 @@
+function compareNumbers (num1, num2) {
+    if (num1 > num2) {
+        return console.log("True");
+    } else {
+        return console.log("False");
+    }
+}
+
+compareNumbers(234, 333);
+compareNumbers(234, 33);
+compareNumbers(4, 33);
